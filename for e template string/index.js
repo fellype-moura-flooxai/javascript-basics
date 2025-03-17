80de8fd2d858e4a56a557ebd40852939d3c3bf35
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-03-17 18:37:09 -0300

    feat: add folders with examples of for and if/else

---
+++ for e template string/index.js
@@ -0,0 +1,15 @@
+//for e template string usando `${}` 
+
+let n = 10
+
+for (let i =0; i <= 10; i++) {
+    console.log(`${i} X ${n} = ${i*n}`);
+/*sem template string 
+ console.log(i + " X " + n + " = " + (i*n));*/
+}
+
+
+
+    
+
+
---
+++ if else/index.js
@@ -0,0 +1,28 @@
+//if else 
+
+
+// exemplo1
+const a = 20;
+const b = "20"
+
+if (a==b) {console.log("isso ai, comparamos os tipos e deu verdadeiro")
+    
+} else { console.log("Os valores não são iguais, deu falso.")
+    
+}
+
+//exemplo2
+if (a===b) {console.log("isso ai, comparamos os tipos e deu verdadeiro")
+    
+} else { console.log("Os valores não são iguais, deu falso.")
+    
+}
+
+//exemplo3
+if (a === 20) {
+    console.log("O valor de 'a' é exatamente 20.");
+} else if (a > 20) {
+    console.log("O valor de 'a' é maior que 20.");
+} else {
+    console.log("O valor de 'a' é menor que 20.");
+}
